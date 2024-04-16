b11dad67614e85b3dd2e3de3361a76b6d9a35adf
ramisaferdous <rifahramisa@iut-dhaka.edu>
2024-04-17 03:30:53 +0600

    added UploadThing & Image Form,Course Category Form and Course Price Form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -2,15 +2,18 @@ import { IconBadge } from "@/components/ui/icon-badge";
 
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
-import { LayoutDashboard } from "lucide-react";
+import { CircleDollarSign, LayoutDashboard, ListChecks } from "lucide-react";
 import { redirect } from "next/navigation";
 
 import { TitleForm } from "./_components/title-form";
 import { DescriptionForm } from "./_components/description-form";
+import { ImageForm } from "./_components/image-form";
+import { CategoryForm } from "./_components/category-form";
+import { PriceForm } from "./_components/price-form";
 
 
 
-const CourseIdPage= async ({
+const CourseIdPage = async ({
     params
 }: {
     params: { courseId: string }
@@ -26,7 +29,15 @@ const CourseIdPage= async ({
             id: params.courseId
         }
     });
-    
+
+    const categories = await db.category.findMany({
+        orderBy: {
+            name: "asc",
+        },
+    });
+
+    console.log(categories)
+
 
     if (!course) {
         return redirect("/");
@@ -52,38 +63,73 @@ const CourseIdPage= async ({
 
     return (
         <div className="p-6">
-                <div className="flex items-center justify-between">
-                    <div className="flex flex-col gap-y-2">
-                        <h1 className="text-2xl font-medium">
-                            Course setup
-                        </h1>
-                        <span className="text-sm text-slate-700">
-                            Complete all fields {completionText}
-                        </span>
-                    </div>
+            <div className="flex items-center justify-between">
+                <div className="flex flex-col gap-y-2">
+                    <h1 className="text-2xl font-medium">
+                        Course setup
+                    </h1>
+                    <span className="text-sm text-slate-700">
+                        Complete all fields {completionText}
+                    </span>
                 </div>
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+                <div>
+                    <div className="flex items-center gap-x-2">
+                        <IconBadge size="sm" variant="success" icon={LayoutDashboard} />
+                        <h2 className="text-xl">
+                            Customize your course
+                        </h2>
+                    </div>
+                    <TitleForm
+                        initialData={course}
+                        courseId={course.id}
+
+                    />
+                    <DescriptionForm
+                        initialData={course}
+                        courseId={course.id}
+                    />
+                    <ImageForm
+                        initialData={course}
+                        courseId={course.id}
+                    />
+                    <CategoryForm
+                        initialData={course}
+                        courseId={course.id}
+                        options={categories.map((category) => ({
+                            label: category.name,
+                            value: category.id,
+                        }))}
+                    />
+
+                </div>
+                <div className="space-y-6">
                     <div>
                         <div className="flex items-center gap-x-2">
-                            <IconBadge size="sm" variant="success" icon ={LayoutDashboard} />
-                            <h2 className="text-xl">
-                                Customize your course
-                            </h2>
+                            <IconBadge icon={ListChecks} />
+                             <h2 className="text-xl">
+                                Course chapters
+                             </h2>
                         </div>
-                        <TitleForm 
-                        initialData = {course} 
-                        courseId = {course.id}
+                        <div>
+                            TODO:Chapters
+                        </div>
 
-                        />
-                        <DescriptionForm
-                            initialData = {course}
-                            courseId={course.id}
-                        />
-                        
                     </div>
+                    <div className="flex items-center gap-x-2">
+                        <IconBadge icon={CircleDollarSign}/>
+                        <h2 className="text-xl">
+                            Sell your course
+                        </h2>
+                    </div>
+                    <PriceForm
+                    initialData={course}
+                    courseId={course.id}/>
                 </div>
+            </div>
         </div>
-      );
+    );
 }
- 
+
 export default CourseIdPage;
