a7358235179a5224862fe04023b99c7f45184f41
Nafisa Kibria <nafisa21@iut-dhaka.edu>
2024-04-22 19:58:53 +0600

    created search query

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -4,6 +4,7 @@ import { usePathname, useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import {  LogOut } from "lucide-react";
 import Link from "next/link";
+import { SearchInput } from "./search-input";
 
 export const NavbarRoutes = () => {
     const pathname = usePathname();
@@ -11,7 +12,14 @@ export const NavbarRoutes = () => {
 
     const isTeacherPage = pathname?.startsWith("/teacher");
     const isPlayerPage = pathname?.includes("/chapter");
+    const isSearchPage= pathname==="/search";
     return (
+        <>
+      {isSearchPage && (
+        <div className="hidden md:block">
+          <SearchInput />
+        </div>
+      )}
         <div className="flex gap-x-2 ml-auto">
             {isTeacherPage || isPlayerPage ? (
                   <Link href="/">
@@ -32,5 +40,6 @@ export const NavbarRoutes = () => {
             
 
         </div>
+        </>
     )
 }
