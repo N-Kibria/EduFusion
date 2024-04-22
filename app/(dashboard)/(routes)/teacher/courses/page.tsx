9c82fd2ea76044f5d7f3ffd11209dd573596d7c9
Nafisa Kibria <nafisa21@iut-dhaka.edu>
2024-04-22 19:13:55 +0600

    created course data table

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,16 +1,33 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-const CoursesPage = () => {
-    return ( 
-        <div className="p-6">
-            <Link href="/teacher/create">
-           <Button variant="purple">
-            Create a new course
-           </Button>
-           </Link>
-        </div>
-     );
+import { db } from "@/lib/db";
+
+
+import { columns } from "./_components/columns";
+import { DataTable } from "./_components/data-table";
+
+const CoursesPage = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const courses = await db.course.findMany({
+    where: {
+      userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return ( 
+    <div className="p-6">
+      <DataTable columns={columns} data={courses} />
+    </div>
+   );
 }
  
 export default CoursesPage;
